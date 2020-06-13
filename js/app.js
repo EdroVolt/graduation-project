d5594a3c07fc69d0ea384df5ce4ecd219e638ed2
Edro <ahmededrees30@gmail.com>
2020-06-13 13:00:10 +0200

    feat: add animation to doctor cards

--- js/app.js
+++ js/app.js
@@ -9,75 +9,80 @@ const fr = document.createDocumentFragment();
 btnDoctor.addEventListener("click", loadDoctors);
 
 function loadDoctors() {
-    divContent.textContent = "";
+  divContent.textContent = "";
 
-    for (let i = 0; i < 50; i++) {
-        // create doctor card
-        const divCard = document.createElement("div");
-        divCard.classList.add("card", "m-auto");
-        divCard.style = "max-width: 540px;";
+  for (let i = 0; i < 50; i++) {
+    // create doctor card
+    const divCard = document.createElement("div");
+    divCard.classList.add(
+      "card",
+      "m-auto",
+      "animate__animated",
+      "animate__zoomIn"
+    );
+    divCard.style = "max-width: 540px;";
 
-        const divRow = document.createElement("div");
-        divRow.classList.add("row", "no-gutters");
+    const divRow = document.createElement("div");
+    divRow.classList.add("row", "no-gutters");
 
-        const divImgCol = document.createElement("div");
-        divImgCol.classList.add("col-md-4");
+    const divImgCol = document.createElement("div");
+    divImgCol.classList.add("col-md-4");
 
-        const img = document.createElement("img");
-        img.setAttribute("src", "imgs/doctor2.jpg");
-        img.classList.add("card-img");
+    const img = document.createElement("img");
+    img.setAttribute("src", "imgs/doctor2.jpg");
+    img.classList.add("card-img");
 
+    const divBodyCol = document.createElement("div");
+    divBodyCol.classList.add("col-md-8");
 
-        const divBodyCol = document.createElement("div");
-        divBodyCol.classList.add("col-md-8");
+    const divBody = document.createElement("div");
+    divBody.classList.add("card-body");
 
-        const divBody = document.createElement("div");
-        divBody.classList.add("card-body");
+    const h5Body = document.createElement("h5");
+    h5Body.classList.add("card-title");
+    h5Body.textContent = "Card title";
 
-        const h5Body = document.createElement("h5");
-        h5Body.classList.add("card-title");
-        h5Body.textContent = "Card title";
+    const p1Body = document.createElement("p");
+    p1Body.classList.add("card-text");
+    p1Body.textContent =
+      "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.";
 
-        const p1Body = document.createElement("p");
-        p1Body.classList.add("card-text");
-        p1Body.textContent = "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.";
+    const p2Body = document.createElement("p");
+    p2Body.classList.add("card-text");
 
-        const p2Body = document.createElement("p");
-        p2Body.classList.add("card-text");
+    const small = document.createElement("small");
+    small.classList.add("text-muted");
+    small.textContent = "Last updated 3 mins ago";
 
-        const small = document.createElement("small");
-        small.classList.add("text-muted");
-        small.textContent = "Last updated 3 mins ago";
+    // append element to create card tree
+    divCard.appendChild(divRow);
 
-        // append element to create card tree
-        divCard.appendChild(divRow);
+    divRow.appendChild(divImgCol);
+    divRow.appendChild(divBodyCol);
 
-        divRow.appendChild(divImgCol);
-        divRow.appendChild(divBodyCol);
+    divImgCol.appendChild(img);
 
-        divImgCol.appendChild(img);
+    divBodyCol.appendChild(divBody);
 
-        divBodyCol.appendChild(divBody);
+    divBody.appendChild(h5Body);
+    divBody.appendChild(p1Body);
+    divBody.appendChild(p2Body);
 
-        divBody.appendChild(h5Body);
-        divBody.appendChild(p1Body);
-        divBody.appendChild(p2Body);
+    p2Body.appendChild(small);
 
-        p2Body.appendChild(small);
-
-        // append card to document fragment
-        fr.appendChild(divCard);
-    }
-    divContent.appendChild(fr);
-    fr.textContent = "";
+    // append card to document fragment
+    fr.appendChild(divCard);
+  }
+  divContent.appendChild(fr);
+  fr.textContent = "";
 }
 
 // view patients
 btnPatient.addEventListener("click", loadPatients);
 
 function loadPatients() {
-    divContent.textContent = "";
+  divContent.textContent = "";
 
-    //   for (let i = 0; i < 50; i++) {}
-    //   divContent.appendChild(fr);
+  //   for (let i = 0; i < 50; i++) {}
+  //   divContent.appendChild(fr);
 }
