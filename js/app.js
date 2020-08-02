b4399e7b5f6ac1f43cf5d3ef8ba35088dd06c860
Edro <ahmededrees30@gmail.com>
2020-08-03 00:20:07 +0200

    feat: add clinics functionalities && make page responsieve

--- js/app.js
+++ js/app.js
@@ -5,8 +5,16 @@ const divContent = document.querySelector(".my-content");
 
 const fr = document.createDocumentFragment();
 
+document.body.onload = loadDoctors;
+
 // view doctors
 btnDoctor.addEventListener("click", loadDoctors);
+
+// view patients
+btnPatient.addEventListener("click", loadPatients);
+
+// view clinics
+btnClinic.addEventListener("click", loadClinics);
 
 function loadDoctors() {
   divContent.textContent = "";
@@ -80,9 +88,6 @@ function loadDoctors() {
   divContent.appendChild(divCardGrid);
   //   fr.textContent = "";
 }
-
-// view patients
-btnPatient.addEventListener("click", loadPatients);
 
 function loadPatients() {
   divContent.textContent = "";
@@ -163,4 +168,77 @@ function loadPatients() {
 
   divContent.appendChild(divTableGrid);
   //   divContent.textContent = "";
+}
+
+function loadClinics() {
+  divContent.textContent = "";
+
+  const divCardGrid = document.createElement("div");
+  divCardGrid.classList.add("card-grid");
+
+  for (let i = 0; i < 50; i++) {
+    // create doctor card
+    const divCard = document.createElement("div");
+    divCard.classList.add(
+      "card",
+      "m-auto",
+      "animate__animated",
+      "animate__zoomIn"
+    );
+    divCard.style = "max-width: 540px;";
+
+    const divRow = document.createElement("div");
+    divRow.classList.add("row", "no-gutters");
+
+    const divImgCol = document.createElement("div");
+    divImgCol.classList.add("col-md-4");
+
+    const img = document.createElement("img");
+    img.setAttribute("src", "imgs/clinic.jpg");
+    img.classList.add("card-img", "h-100");
+
+    const divBodyCol = document.createElement("div");
+    divBodyCol.classList.add("col-md-8");
+
+    const divBody = document.createElement("div");
+    divBody.classList.add("card-body");
+
+    const h5Body = document.createElement("h5");
+    h5Body.classList.add("card-title");
+    h5Body.textContent = "Card title";
+
+    const p1Body = document.createElement("p");
+    p1Body.classList.add("card-text");
+    p1Body.textContent =
+      "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.";
+
+    const p2Body = document.createElement("p");
+    p2Body.classList.add("card-text");
+
+    const aDetails = document.createElement("a");
+    aDetails.classList.add("btn", "btn-primary");
+    aDetails.textContent = "Details";
+    aDetails.setAttribute("href", "#");
+
+    // append element to create card tree
+    divCard.appendChild(divRow);
+
+    divRow.appendChild(divImgCol);
+    divRow.appendChild(divBodyCol);
+
+    divImgCol.appendChild(img);
+
+    divBodyCol.appendChild(divBody);
+
+    divBody.appendChild(h5Body);
+    divBody.appendChild(p1Body);
+    divBody.appendChild(p2Body);
+
+    p2Body.appendChild(aDetails);
+
+    // append card to grid div
+    divCardGrid.appendChild(divCard);
+  }
+  divContent.appendChild(divCardGrid);
+  //   fr.textContent = "";
 }
